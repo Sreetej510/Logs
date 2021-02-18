23b0ac9f15fc282d6658570b2d83a9fd144aa088
sree tej <sreetej510@gmail.com>
2021-02-18 22:20:54 +0530

    Logs

---
+++ public/JS/mainScript.js
@@ -0,0 +1,42 @@
+window.onload = function () {
+    elements = document.getElementsByClassName('collapsed');
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].onclick = function () { collapsedFunction(this); };
+    }
+}
+
+
+function collapsedFunction(e) {
+    var collapseId = e.getAttribute('data-target');
+    var item = document.getElementById(collapseId);
+
+    var isShown = item.classList.contains('show');
+    if (!isShown) {
+        item.style.visibility = 'hidden';
+        item.classList.remove('collapse');
+        var height = item.offsetHeight;
+        item.classList.add('collapsing');
+        setTimeout(function () { item.setAttribute('style', 'height: ' + height + 'px'); }, 1);
+        setTimeout(function () {
+            item.classList.remove('collapsing');
+            item.classList.add('collapse', 'show');
+        }, 350);
+    } else {
+        item.classList.remove('show', 'collapse');
+        item.classList.add('collapsing');
+        setTimeout(function () { item.setAttribute('style', 'height: 0px'); }, 1);
+        setTimeout(function () {
+            item.classList.remove('collapsing')
+            item.removeAttribute('style');
+            item.classList.add('collapse');
+        }, 350);
+
+    }
+}
+
+function menu() {
+    document.getElementById('customNav').classList.toggle('hamClick');
+    document.getElementById('navOverlay').classList.toggle('d-none');
+    document.getElementById('ham').classList.toggle('active');
+    document.getElementById('header').classList.toggle('opacity-0h5');
+}
